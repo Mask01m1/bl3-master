01913821b12a505bab30fb7d0e862214496d7fd1
Mask <Mask@MY-SKPC>
2019-04-08 11:29:43 +0300

    MyTask 3

--- B-3-Operators/Practice.cs
+++ B-3-Operators/Practice.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.CSharp.RuntimeBinder;
 
 namespace B_3_Operators
 {
@@ -12,8 +14,13 @@ namespace B_3_Operators
         /// B3-P1/5. NumbersAddition. Напишите алгоритм, который складывает два числа.
         /// </summary>
         public static void B3_P1_9_NumbersAddition()
-        {
-
+        {   
+            Console.Write("Ведите первое число: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Ведите второе число: ");
+            int b = int.Parse(Console.ReadLine());
+            int sum = a + b;
+            Console.WriteLine($"Сумма чичел = {sum}");
         }
 
         /// <summary>
@@ -21,10 +28,28 @@ namespace B_3_Operators
         /// Пускай он теперь не выводит ответ сам. 
         /// А запрашивает ответ и потом проверяет его на правильность.
         /// </summary>
-       /// </summary>
         public static void B3_P2_9_CheckResultAddition()
         {
+            
+            Console.Write("Ведите первое число: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Ведите второе число: ");
+            int b = int.Parse(Console.ReadLine());
+            int sum = a + b;
 
+            startloop:
+                Console.WriteLine("Введите сумму");
+                string answe = Console.ReadLine();
+                int sum2 = Convert.ToInt32(answe);
+                if (sum2 == sum)
+                {
+                    Console.WriteLine($"Ваш ответ верный сумма чичел = {sum}");
+                }
+                else
+                {
+                    Console.WriteLine("Ответ неверный");
+                    goto startloop;
+                }
         }
 
         /// <summary>
@@ -35,7 +60,30 @@ namespace B_3_Operators
         public static void B3_P3_9_CheckResultAdditionWithTips()
         {
 
+            Console.Write("Ведите первое число: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Ведите второе число: ");
+            int b = int.Parse(Console.ReadLine());
+            int sum = a + b;
+            startloop:
+            Console.WriteLine("Введите сумму");
+            string answe = Console.ReadLine();
+            int sum2 = Convert.ToInt32(answe);
+            if (sum2 == sum)
+                Console.WriteLine($"Ваш ответ верный сумма чичел = {sum}");
+            else
+            {
+                if (sum2 < sum)
+                    Console.WriteLine("Ответ неверный занчение меньше");
+                else
+                {
+                    Console.WriteLine("Ответ неверный занчение больше");
+                }
+
+                goto startloop;
+            }
         }
+
 
         /// <summary>
         /// B3-P4/9. CheckResultWithOperator. Изменить предыдущий алгоритм. 
@@ -43,8 +91,56 @@ namespace B_3_Operators
         /// </summary>
         public static void B3_P4_9_CheckResultWithOperator()
         {
+            string oper1 = "+";
+            string oper2 = "-";
+            int sum, sum2;
 
+            Console.Write("Ведите первое число: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Ведите второе число: ");
+            int b = int.Parse(Console.ReadLine());
+
+            opers:
+            Console.WriteLine("Введите оператор \"+\" или \"-\" :");
+            string oper = Console.ReadLine();
+            if ((oper == oper1) && (oper != null))
+                sum = a+b;
+            else
+            {
+                if ((oper == oper2) && (oper != null))
+                    sum = a-b;
+                else
+                {
+                    Console.WriteLine("Вы ввели неправильный оператор или пустое значение");
+            goto opers;
+                }
+            }
+            startloop:
+            Console.WriteLine($"Введите сумму");
+            string answe = Console.ReadLine();
+            bool isInt = int.TryParse(answe, out sum2);
+            if (isInt)
+            {
+                sum2 = Convert.ToInt32(answe);
+            }
+            else
+            {
+                Console.WriteLine($"ввели не число");
+                goto startloop;
+            }
+
+            if (sum2 == sum)
+                Console.WriteLine($"Ваш ответ верный сумма чичел равна:{sum}");
+            else
+            {
+                if (sum2 < sum)
+                    Console.WriteLine("Ответ неверный занчение меньше");
+                else
+                    Console.WriteLine("Ответ неверный занчение больше");
+            goto startloop;
+            }
         }
+
 
         /// <summary>
         /// B3-P5/9. CheckResultWithAttemps. Изменить предыдущий алгоритм. 
@@ -52,8 +148,61 @@ namespace B_3_Operators
         /// </summary>
         public static void B3_P5_9_CheckResultWithAttemps()
         {
+            string oper1 = "+";
+            string oper2 = "-";
+            int sum, sum2;
 
+            Console.Write("Ведите первое число: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Ведите второе число: ");
+            int b = int.Parse(Console.ReadLine());
+
+        opers:
+            Console.WriteLine("Введите оператор \"+\" или \"-\" :");
+            string oper = Console.ReadLine();
+            if ((oper == oper1) && (oper != null))
+                sum = a + b;
+            else
+            {
+                if ((oper == oper2) && (oper != null))
+                    sum = a - b;
+                else
+                {
+                    Console.WriteLine("Вы ввели неправильный оператор или пустое значение");
+                    goto opers;
+                }
+            }
+            
+            for (int i = 0; i <3; i++)
+            {
+            startloop:
+                Console.WriteLine($"Введите сумму");
+                string answe = Console.ReadLine();
+                bool isInt = int.TryParse(answe, out sum2);
+                if (isInt)
+                {
+                    sum2 = Convert.ToInt32(answe);
+                }
+                else
+                {
+                    Console.WriteLine($"ввели не число");
+                    goto startloop;
+                }
+
+                if (sum2 == sum)
+                    Console.WriteLine($"Ваш ответ верный сумма чичел равна:{sum}");
+                else
+                {
+                    if (sum2 < sum)
+                        Console.WriteLine("Ответ неверный занчение меньше");
+                    else
+                        Console.WriteLine("Ответ неверный занчение больше");
+                    goto startloop;
+                }
+            }
         }
+        
+
 
         /// <summary>
         /// B3-P6/9. FiveNumbersAddition. Изменить предыдущий алгоритм. 
@@ -61,7 +210,67 @@ namespace B_3_Operators
         /// </summary>
         public static void B3_P6_9_FiveNumbersAddition()
         {
+            string oper1 = "+";
+            string oper2 = "-";
+            int sum, sum2;
 
+            Console.Write("Ведите первое число: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Ведите второе число: ");
+            int b = int.Parse(Console.ReadLine());
+            Console.Write("Ведите третье число: ");
+            int c = int.Parse(Console.ReadLine());
+            Console.Write("Ведите четвёртое число: ");
+            int d = int.Parse(Console.ReadLine());
+            Console.Write("Ведите пятое число: ");
+            int e = int.Parse(Console.ReadLine());
+
+        opers:
+            Console.WriteLine("Введите оператор \"+\" или \"-\" :");
+            string oper = Console.ReadLine();
+            if ((oper == oper1) && (oper != null))
+                sum = a + b + c + d + e;
+            else
+            {
+                if ((oper == oper2) && (oper != null))
+                    sum = a - b - c - d - e;
+                else
+                {
+                    Console.WriteLine("Вы ввели неправильный оператор или пустое значение");
+                    goto opers;
+                }
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                startloop:
+                Console.WriteLine($"Введите сумму");
+                string answe = Console.ReadLine();
+                bool isInt = int.TryParse(answe, out sum2);
+                if (isInt)
+                {
+                    sum2 = Convert.ToInt32(answe);
+                }
+                else
+                {
+                    Console.WriteLine($"ввели не число");
+                    goto startloop;
+                }
+
+                if (sum2 == sum)
+                {
+                    Console.WriteLine($"Ваш ответ верный сумма чичел равна:{sum}");
+                    break;
+                }
+                else
+                {
+                    if (sum2 < sum)
+                        Console.WriteLine("Ответ неверный занчение меньше");
+                    else
+                        Console.WriteLine("Ответ неверный занчение больше");
+                    goto startloop;
+                }
+            }
         }
 
         /// <summary>
@@ -70,7 +279,67 @@ namespace B_3_Operators
         /// </summary>
         public static void B3_P7_9_NumbersResultWithInfoIfCorrect()
         {
+            string oper1 = "+";
+            string oper2 = "-";
+            int sum, sum2;
 
+            Console.Write("Ведите первое число: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Ведите второе число: ");
+            int b = int.Parse(Console.ReadLine());
+            Console.Write("Ведите третье число: ");
+            int c = int.Parse(Console.ReadLine());
+            Console.Write("Ведите четвёртое число: ");
+            int d = int.Parse(Console.ReadLine());
+            Console.Write("Ведите пятое число: ");
+            int e = int.Parse(Console.ReadLine());
+
+        opers:
+            Console.WriteLine("Введите оператор \"+\" или \"-\" :");
+            string oper = Console.ReadLine();
+            if ((oper == oper1) && (oper != null))
+                sum = a + b + c + d + e;
+            else
+            {
+                if ((oper == oper2) && (oper != null))
+                    sum = a - b - c - d - e;
+                else
+                {
+                    Console.WriteLine("Вы ввели неправильный оператор или пустое значение");
+                    goto opers;
+                }
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+            startloop:
+                Console.WriteLine($"Введите сумму");
+                string answe = Console.ReadLine();
+                bool isInt = int.TryParse(answe, out sum2);
+                if (isInt)
+                {
+                    sum2 = Convert.ToInt32(answe);
+                }
+                else
+                {
+                    Console.WriteLine($"ввели не число");
+                    goto startloop;
+                }
+
+                if (sum2 == sum)
+                {
+                    Console.WriteLine($"Ваш ответ верный сумма чичел равна:{sum}. Ваш ответ: {sum2}");
+                    break;
+                }
+                else
+                {
+                    if (sum2 < sum)
+                        Console.WriteLine("Ответ неверный занчение меньше");
+                    else
+                        Console.WriteLine("Ответ неверный занчение больше");
+                    goto startloop;
+                }
+            }
         }
 
         /// <summary>
@@ -78,7 +347,21 @@ namespace B_3_Operators
         /// </summary>
         public static void B3_P8_9_CircleArea()
         {
-
+            double s;
+            startloop:
+            Console.Write("Введите радиус: ");
+            string temp = Console.ReadLine();
+            if (temp.Contains('.'))
+            {
+                Console.WriteLine(@"укажите разделитель"","" f а не "".""");
+                goto startloop;
+            }
+            else
+            {
+                double r = double.Parse(temp);
+                s = Math.PI * Math.Pow(r, 2);
+                Console.WriteLine($"Плошадь круга с радуисом {r} равна: {s}");
+            }
         }
 
         /// <summary>
@@ -86,7 +369,43 @@ namespace B_3_Operators
         /// </summary>
         public static void B3_P9_9_CreaditCalculator()
         {
+            double rate;        // Годовая процентная ставка по кредиту
+            double loanservece; // Процентная ставка в месяц
+            decimal sum;            // Сумма по кредиту
+            double term ;           // Срок на который берется кредит
+            decimal k;           // коэффициента аннуитета   
+            decimal pay;         // месячный платёж
+            decimal sum1;        // сумма процентов за первый месяц
+            decimal loann;       // остаток кредита по месяцам
+            decimal balacepay;   // сумма погашения основного долга
 
+            Console.Write("Введите срок кредита в месяцах:" );
+            term = double.Parse(Console.ReadLine());
+            Console.Write("Введите сумму кредита: ");
+            sum = decimal.Parse(Console.ReadLine());
+            Console.Write("Введите годовую процентную ставку: ");
+            rate = double.Parse(Console.ReadLine());
+
+            loanservece = rate / 1200;
+            decimal loans = Convert.ToDecimal(loanservece);
+
+            double temp = Math.Pow((1 + loanservece), term);
+            decimal t = Convert.ToDecimal(temp);
+            
+            k= (loans * t) / (t - 1);
+            pay = k * sum;
+            Console.WriteLine(term);
+            while (term > 0)
+            {
+
+                sum1 = loans * sum;
+                loann = pay - sum1;
+                balacepay = sum - loann;
+                sum = balacepay;
+                ;
+                term--;
+                Console.WriteLine($"месяц:{term}   Остаток: {balacepay}   Погашение % {sum1}   Погашение осн. долга {loann}   Месячный платёж {pay}");
+            }
         }
     }
 }
